9debd0d72d5e859a827c99a98b47128cb06eab4b
Crosline <cagkancaglayanel@gmail.com>
2024-01-17 01:24:31 +0300

    Add: simple screen manager

--- Assets/_Scripts/Managers/ScreenManager.cs
+++ Assets/_Scripts/Managers/ScreenManager.cs
@@ -1,18 +1,48 @@
-﻿using Game.Systems;
+﻿using System;
+using System.Collections.Generic;
+using Game.Systems;
 using UnityEngine;
+using Screen = Game.Screens.Screen;
 
 namespace Game.Managers
 {
     public class ScreenManager : GameSystem
     {
+        [SerializeField] private List<Screen> _screens;
+        public static event Action<string> OnScreenOpened;
+        public static event Action<string> OnScreenClosed;
+        private Stack<Screen> _activeScreens;
         public override void Initialize()
         {
-            Debug.Log("YE1");
+            _activeScreens = new Stack<Screen>();
         }
 
         public override void Dispose()
         {
-            Debug.Log("YE2");
+            _activeScreens.Clear();
+        }
+
+        public void OpenScreen(string screenName)
+        {
+            foreach (var screen in _screens)
+            {
+                if (!screenName.Equals(screen.name)) continue;
+
+                screen.OnOpen();
+                OnScreenOpened?.Invoke(screenName);
+                break;
+            }
+        }
+        public void CloseActiveScreen()
+        {
+            var activeScreen = _activeScreens.Pop();
+            
+            activeScreen.OnClose();
+            OnScreenClosed?.Invoke(activeScreen.name);
+        }
+        public void CloseActiveScreenWithAnimation()
+        {
+            
         }
     }
 }
