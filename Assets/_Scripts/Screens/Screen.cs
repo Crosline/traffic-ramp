b3e41580d9b1052bca9e252a5f631d6bc3c5c9bf
Crosline <cagkancaglayanel@gmail.com>
2024-01-17 01:24:15 +0300

    Add: Screens

---
+++ Assets/_Scripts/Screens/Screen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Screens
+{
+    public abstract class Screen : MonoBehaviour
+    {
+        public abstract void OnOpen();
+        public abstract void OnClose();
+    }
+}
---
+++ Assets/_Scripts/Screens/UpgradeScreen.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Screens
+{
+    public class UpgradeScreen : Screen
+    {
+        public override void OnOpen()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnClose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
