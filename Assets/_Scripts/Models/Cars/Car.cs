478c2e9ff4986f3561b18fa6d142d1e4bd00fe26
Crosline <cagkancaglayanel@gmail.com>
2024-01-20 16:43:13 +0300

    Add: Some car data

---
+++ Assets/_Scripts/Models/Cars/Car.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.Models.Cars
+{
+    [CreateAssetMenu(fileName = "Car", menuName = "Game/Car/Default")]
+    public class Car : ScriptableObject
+    {
+        [field: SerializeField]
+        public CarModel Id { get; private set; }
+        [field: SerializeField]
+        public GameObject Prefab { get; private set; }
+        [field: SerializeField]
+        public float BaseSpeed { get; private set; }
+    }
+
+
+    public enum CarModel : byte
+    {
+        Sedan = 100,
+        SedanSports = 105,
+        Racer = 110,
+        RacerFuturistic = 150,
+    }
+}
---
+++ Assets/_Scripts/Models/Cars/PlayerCar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game.Models.Cars
+{
+    [CreateAssetMenu(fileName = "Player_Car", menuName = "Game/Car/Player")]
+    public class PlayerCar : Car
+    {
+        [field: SerializeField]
+        public float Smoothness { get; private set; }
+    }
+}
